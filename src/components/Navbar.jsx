e0bf4fb652dabe676b83867eeea9b618404b8d3f
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-02-04 18:17:27 +0530

    updated assets folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import img from '../assets/planet.png';
+
 
 export default function Navbar() {
   return (
     <nav className="navbar">
          <div className="logo">
             <h1 className="app-title">Space Traveller Hub</h1>
-            <img src="src/assets/planet.png" alt="space pic"/>
+            <img src={img} alt="space pic"/>
          </div>
          <ul className="navlinks">
             <li><NavLink to="/" className="link">Rocket</NavLink></li>
