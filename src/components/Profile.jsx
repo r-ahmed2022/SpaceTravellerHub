f0bee7cfcb081165e27e551851f1991c0ddc04f9
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-02-05 12:10:10 +0530

    updated .js

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { JOIN_ROCKET } from '../redux/Rocket/rocket'
 import { JOIN_MISSION } from '../redux/Missions/mission'
 import {useDispatch} from 'react-redux'
 
 export default function Profile() {
   const { list } = useSelector((state) => state?.rocketslist);
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const rocketBookings = list?.map((rocket) => {
     return (!rocket.reserved) ? (<li className="item" key={rocket.id}>{rocket.name}
@@ -34,7 +35,7 @@ export default function Profile() {
 
             <button type="button" className="activebtn" 
              onClick={ () => {
-                   document.location.replace("/mission")
+                   navigate("/mission")
                   }            
               }>Join Mission</button>
             </div> 
@@ -51,7 +52,7 @@ export default function Profile() {
              <h4>Book your favorite rocket</h4>
              <button type="button" className="activebtn" 
              onClick={ () => {
-                   document.location.replace("/")
+                   navigate("/")
                   }            
               }>Reserve Rocket</button></div> 
               : rocketBookings
